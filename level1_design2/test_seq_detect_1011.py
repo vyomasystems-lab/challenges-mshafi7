dac85400d56a43fbe321fe82df943cd37261209d
mshafi7 <mshafi.9198@gmail.com>
2022-07-22 09:08:39 +0000

    Made test for overlapping sequence

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -22,14 +22,42 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)  
     dut.reset.value = 0
     await FallingEdge(dut.clk)
-    
-    await raisingEdge(dut.clk)
+
     dut.inp_bit.value = 1
-    await raisingEdge(dut.clk)
+    await FallingEdge(dut.clk)
     dut.inp_bit.value = 0
-    await raisingEdge(dut.clk)
+    await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
-    await raisingEdge(dut.clk)
+    await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
-    await raisingEdge(dut.clk)
+    await FallingEdge(dut.clk)
     assert dut.seq_seen.value == 1, cocotb.log.info('Sequence detection error')
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    assert dut.seq_seen.value == 1, cocotb.log.info('Sequence detection error')    
